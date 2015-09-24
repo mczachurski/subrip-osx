8a54d4a233b2991689a12aa6e89e3d304f8b4be2
Marcin Czachurski <mczachurski@icloud.com>
2015-09-24 11:57:11 +0200

    Initial Commit

---
+++ Subrip/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Subrip
+//
+//  Created by Marcin Czachurski on 24.09.2015.
+//  Copyright © 2015 SunLine Technologies. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Subrip/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Subrip
+//
+//  Created by Marcin Czachurski on 24.09.2015.
+//  Copyright © 2015 SunLine Technologies. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
