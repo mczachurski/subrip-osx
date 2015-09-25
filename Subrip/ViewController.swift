f3583f79a786983b7f24c69dd2bf7b2d7a1979bf
Marcin Czachurski <mczachurski@icloud.com>
2015-09-25 20:05:07 +0200

    Add steppers.

--- Subrip/ViewController.swift
+++ Subrip/ViewController.swift
@@ -8,18 +8,27 @@
 
 import Cocoa
 
-class ViewController: NSViewController
+class ViewController: NSViewController, NSTextFieldDelegate
 {
     private var filePath:NSURL!
 
     @IBOutlet weak var fileNameOutlet: NSTextField!
     @IBOutlet weak var framesOutlet: NSTextField!
     @IBOutlet weak var convertOutlet: NSButton!
+    @IBOutlet weak var frameStepperOutlet: NSStepper!
+    @IBOutlet weak var offsetOutlet: NSTextField!
+    @IBOutlet weak var offsetStepperOutlet: NSStepper!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         framesOutlet.doubleValue = 23.976
+        offsetOutlet.doubleValue = 0.0
+        frameStepperOutlet.doubleValue = framesOutlet.doubleValue
+        offsetStepperOutlet.doubleValue = offsetOutlet.doubleValue
+        
+        framesOutlet.delegate = self
+        offsetOutlet.delegate = self
     }
 
     func setURL(url:NSURL)
@@ -28,12 +37,34 @@ class ViewController: NSViewController
         fileNameOutlet.stringValue = filePath.lastPathComponent!
         framesOutlet.enabled = true
         convertOutlet.enabled = true
+        frameStepperOutlet.enabled = true
     }
     
     override var representedObject: AnyObject?
     {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    @IBAction func frameStepperAction(sender: NSStepper)
+    {
+        framesOutlet.doubleValue = sender.doubleValue
+    }
+    
+    @IBAction func offsetStepperAction(sender: NSStepper)
+    {
+        offsetOutlet.doubleValue = sender.doubleValue
+    }
+    
+    override func controlTextDidChange(obj: NSNotification)
+    {
+        if(obj.object as? NSTextField == framesOutlet)
+        {
+            frameStepperOutlet.doubleValue = framesOutlet.doubleValue
+        }
+        else if(obj.object as? NSTextField == offsetOutlet)
+        {
+            offsetStepperOutlet.doubleValue = offsetOutlet.doubleValue
         }
     }
     
